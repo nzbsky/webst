02d29e806e4f69ec2fd8230c116b0c1cabc9bace
nzbsky <150057133+nzbsky@users.noreply.github.com>
2025-10-28 20:32:16 +0200

    Add files via upload

---
+++ kava/js/lessn.js
@@ -0,0 +1,13 @@
+
+const myFavoriteGameName = "Minecraft"; 
+
+
+const myFavoriteGameText = "my favorite game's name";
+
+
+const lastCharacter = myFavoriteGameName.charAt(myFavoriteGameName.length - 1);
+
+
+
+
+console.log(`The last letter in ${myFavoriteGameText} ${myFavoriteGameName} is ${lastCharacter} !`);
